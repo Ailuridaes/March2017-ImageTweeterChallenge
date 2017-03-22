c4904452110c4d23f9b723d37805a01e58131b00
Ailuridaes <ailuridaes@gmail.com>
2017-03-21 21:42:49 -0700

    Completed Level 3

--- src/ImageTweeter/LambdaHandler.cs
+++ src/ImageTweeter/LambdaHandler.cs
@@ -14,25 +14,45 @@ using Tweetinvi.Parameters;
 [assembly:LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 namespace ImageTweeter {
     public class LambdaHandler {
-        public string Handler(ILambdaContext context) {
+        public string Handler(S3Event uploadEvent, ILambdaContext context) {
+            string bucket = uploadEvent.Records[0].S3.Bucket.Name;
+            string objectKey = uploadEvent.Records[0].S3.Object.Key;
+            var tempImagePath = $"/tmp/{objectKey}";
 
             // Level 1: Make this output when a file is uploaded to S3
             LambdaLogger.Log("Hello from The AutoMaTweeter!");
-
-            // Level 2: Get the bucket name and key from event data and log to cloudwatch
-            //var bucketName = '';
-            //var keyName = '';
-
-            //LambdaLogger.Log($"The AutoMaTweeter found a file path: {bucketName}/{keyName}");
-
-            // Boss Level: Use Amazon Rekognition to get keywords about the image
-
+            LambdaLogger.Log(uploadEvent.Records.ToJson());
+            LambdaLogger.Log($"Bucket name: {bucket}");
+            LambdaLogger.Log($"Image name: {objectKey}");
+            
+            using (var client = new AmazonS3Client(Amazon.RegionEndpoint.USWest2)) 
+            {
+                S3Model.GetObjectRequest request = new S3Model.GetObjectRequest
+                {
+                    BucketName = bucket,
+                    Key = objectKey
+                };
+                
+                using (S3Model.GetObjectResponse response = client.GetObjectAsync(request).Result)  
+                {
+                    using(var fileStream = File.Create(tempImagePath)) {
+                        response.ResponseStream.CopyTo(fileStream);
+                    }
+                }
+            }
+            
             // Level 3: Post the image and message to twitter
-            //var consumerKey = "GCNunS5DfXGwh8rvFAterxmXP";
-            //var consumerSecret = "fq03tBiAIAM7pB6DjRI8S69scCFiR3FibCbjz3HWfjEOPMLSQD";
-            //var accessToken = "842206967632338944-XMAH3FU86RSak57FVJmglXn4HAvNmpy";
-            //var accessTokenSecret = "fiFvNoEASqyqWo3FuFr5JKBYyWlILihVLlGCTSxfqAtlv";
+            var consumerKey = "GCNunS5DfXGwh8rvFAterxmXP";
+            var consumerSecret = "fq03tBiAIAM7pB6DjRI8S69scCFiR3FibCbjz3HWfjEOPMLSQD";
+            var accessToken = "842206967632338944-XMAH3FU86RSak57FVJmglXn4HAvNmpy";
+            var accessTokenSecret = "fiFvNoEASqyqWo3FuFr5JKBYyWlILihVLlGCTSxfqAtlv";
 
+            byte[] img = File.ReadAllBytes(tempImagePath);
+
+            Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+            
+            // Publish the Tweet
+            Tweet.PublishTweetWithImage("Meow!", img);
             return "Run Complete";
         }
     }
